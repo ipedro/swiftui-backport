b0cd9db84566188e8d7440c59f5bd924fc8ab73d
Pedro Almeida <ip4dro@gmail.com>
2024-11-27 23:05:50 -0300

    invert logic

--- Package.swift
+++ Package.swift
@@ -1,9 +1,8 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let isDevelopment = !Context.packageDirectory.contains("/checkouts/")
+let isSDK = Context.packageDirectory.contains("/checkouts/")
 
 var package = Package(
     name: "swiftui-backport",
@@ -15,19 +14,20 @@ var package = Package(
         .visionOS(.v1),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUIBackport",
             targets: ["SwiftUIBackport"]
         ),
+        .library(
+            name: "SwiftUIBackport_Examples",
+            targets: ["SwiftUIBackport_Examples"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUIBackport",
-            path: isDevelopment ? "Development" : ".",
-            sources: isDevelopment ? nil : ["SwiftUIBackport.swift"]
+            path: isSDK ? "." : "Development", 
+            sources: isSDK ? ["SwiftUIBackport.swift"] : nil
         ),
         .target(
             name: "SwiftUIBackport_Examples",
@@ -36,12 +36,3 @@ var package = Package(
         ),
     ]
 )
-
-if isDevelopment {
-    package.products.append(
-        .library(
-            name: "SwiftUIBackport_Examples",
-            targets: ["SwiftUIBackport_Examples"]
-        )
-    )
-}
